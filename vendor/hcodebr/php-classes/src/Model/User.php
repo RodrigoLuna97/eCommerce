54ece097edf137a7b86658d1e4cadf743ad79436
RodrigoLuna97 <rodrigobpt1@hotmail.com>
2019-03-27 19:57:30 -0300

    alteracao recuperar senha ainda sem exito ainda e divisao das classe e mudancas nos use e include

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -177,6 +177,56 @@ class User extends Model {
 
  }
 
+ public static function validForgotDecrypt($code)
+ {
+     $code = base64_decode($code);
+     $cod = mb_substr($code, openssl_cipher_iv_length('aes-256-cbc'), null, '8bit');
+     $iv = mb_substr($code, 0, openssl_cipher_iv_length('aes-256-cbc'), '8bit');;
+     $idrecovery = openssl_decrypt($cod, 'aes-256-cbc', User::SECRET, 0, $iv);
+     $sql = new Sql();
+     $results = $sql->select("
+         SELECT *
+         FROM tb_userspasswordsrecoveries a
+         INNER JOIN tb_users b USING(iduser)
+         INNER JOIN tb_persons c USING(idperson)
+         WHERE
+         a.idrecovery = :idrecovery
+         AND
+         a.dtrecovery IS NULL
+         AND
+         DATE_ADD(a.dtregister, INTERVAL 1 HOUR) >= NOW();
+     ", array(
+         ":idrecovery"=>$idrecovery
+     ));
+     if (count($results) === 0)
+     {
+         throw new \Exception("Não foi possível recuperar a senha.");
+     }
+     else
+     {
+         return $results[0];
+     }
+ }
+
+    public static function setForgotUsed($idrecovery){
+
+        $sql = new Sql();
+
+        $sql->query("UPDATE tb_userspasswordsrecoveries SET dtrecovery = NOW() WHERE idrecovery = :idrecovery", array(
+            ":idrecovery"=>$idrecovery
+             ));
+         }
+
+
+     public function setPassword($password){
+
+        $sql = new Sql();
+        
+        $sql->query("UPDATE tb_users SET td_password = :password WHERE iduser = :iduser ", array(
+            ":password"=>$password,
+            ":iduser"=>$this->getiduser()
+        ));
+     }
 
 }
 
